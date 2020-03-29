5f09c35f96706691f5dfe9d80f349cd874b798dd
StunnerX123 <muhammad.abdurrohman354@gmail.com>
2020-03-29 13:43:08 +0700

    first commit

---
+++ Provis8_Praktikum1/src/kelasutama.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Luthfi RZ
+ */
+public class kelasutama {
+    public static void main(String[] args) {
+        //System.out.println("Hallo World");
+        //menghidupkan objek
+        //frameutama futama = new frameutama();
+        //futama.tampil("Program Pertama GUI");
+        
+        futama utama = new futama();
+       // utama.setTitle("Pemograman Provis");
+        utama.setVisible(true);
+    }
+}
